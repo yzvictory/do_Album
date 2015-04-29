8e312f61d50fe4ee1eaa65bf88b19a5d361c888a
yangzhen <1784054000@qq.com>
2015-04-29 19:20:38 +0800

    初始化Do_Album，提交源码

---
+++ src/iOS/DoExt/Classes/View/YZAlbumUIImageView.h
@@ -0,0 +1,16 @@
+//
+//  AlbumUIImageView.h
+//  DoAlbum_SM
+//
+//  Created by yz on 15/4/29.
+//  Copyright (c) 2015年 DoExt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AssetsLibrary/AssetsLibrary.h>
+
+@interface YZAlbumUIImageView : UIImageView
+
+@property(nonatomic,strong) ALAsset *asset;
+
+@end
---
+++ src/iOS/DoExt/Classes/View/YZAlbumUIImageView.m
@@ -0,0 +1,21 @@
+//
+//  AlbumUIImageView.m
+//  DoAlbum_SM
+//
+//  Created by yz on 15/4/29.
+//  Copyright (c) 2015年 DoExt. All rights reserved.
+//
+
+#import "YZAlbumUIImageView.h"
+
+@implementation YZAlbumUIImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
